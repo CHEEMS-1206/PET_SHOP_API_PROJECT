64a2cbbbbc656e612ffae97559c2428311301b30
PRIYANSHU SINGH <sentysingh456@gmail.com>
2023-04-14 18:23:37 +0530

    wrote controller functions for CRUD at home - '/' after URL 'http://localhost:5000'

---
+++ controllers/home.js
@@ -0,0 +1,17 @@
+// Controllers for home '/' after URL => http://localhost:5000
+
+export const hitHome = (req, res) => {
+  res.send("Welcome to PET SHOP ...");
+}
+
+export const postHome = (req, res) => {
+    res.send("Invalid Request !");
+};
+
+export const updateHome = (req, res) => {
+  res.send("Invalid Request !");
+};
+
+export const deleteHome = (req, res) => {
+    res.send("Invalid Request !");
+};
