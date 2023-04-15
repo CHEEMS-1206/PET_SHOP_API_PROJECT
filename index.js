0b6001f67a67a41601634d408aaaa668d8302059
PRIYANSHU SINGH <sentysingh456@gmail.com>
2023-04-15 08:22:26 +0530

    Created new pet animal option ! > created middleware for 'hamsters' inindex.js > created routes for 'hamster' in routes/apiRoutes/hamster.js > created model for 'hamster' in models/hamster.js > created controllers for CRUD operations on 'hamster' in controllers/apiControllers/hamster.js

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ import cors from 'cors';
 // importing all routers
 import homeRouter from "./routes/home.js";
 import rabbitRouter from "./routes/apiRoutes/Rabbit.js";
+import hamsterRouter from "./routes/apiRoutes/Hamster.js";
 
 // importing database code and rendering for config folder 
 import './config/db.js';
@@ -20,6 +21,7 @@ app.use(express.json());
 //MIDDLEWARES
 app.use('/',homeRouter);
 app.use('/rabbit',rabbitRouter)
+app.use("/hamster", hamsterRouter);
 
 // defining port for backend rest server
 const PORT = 5000;
